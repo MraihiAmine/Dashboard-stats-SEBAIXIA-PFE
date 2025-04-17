440d3e4884deda3842dc03afc4963719ecd68a7d
Amine Mraihi <mraihiamin@gmail.com>
2025-04-17 14:16:42 +0100

    add

---
+++ backend/src/main/java/com/lapeyre/dashboard/nectartools/DashboardStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.lapeyre.dashboard.nectartools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardStatisticsApplication.class, args);
+	}
+
+}
---
+++ nectartools/src/test/java/com/lapeyre/dashboard/nectartools/DashboardStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapeyre.dashboard.nectartools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
